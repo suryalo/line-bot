ed2c009e0f9006dfea1c83be42ade2c2535424e9
Surya <amy.renaissance@gmail.com>
2022-04-11 20:00:40 +0800

    change sticker

--- app.py
+++ app.py
@@ -42,13 +42,13 @@ def handle_message(event):
 
     if '貼圖' in msg:
         sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
+            package_id='2',
+            sticker_id='26'
         )
 
         line_bot_api.reply_message(
-        event.reply_token,
-        sticker_message)
+            event.reply_token,
+            sticker_message)
         return
 
     if msg in ['hi','Hi']:
