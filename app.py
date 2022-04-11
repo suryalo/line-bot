ddc1989c847f887561545185088a00716c1942f9
Surya <amy.renaissance@gmail.com>
2022-04-11 18:39:16 +0800

    add msg

--- app.py
+++ app.py
@@ -38,9 +38,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    r = '抱歉，這個問題不在我的守備範圍！'
+
+    if msg == 'hi':
+        r == 'Hello, 我是機器人Surya！'
+    elif msg == '你吃飯了嗎？';
+        r == '還沒'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='Hello, 我是機器人Surya!'))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
